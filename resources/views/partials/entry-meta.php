1077dfe8ffc3fa9fd554467628743bd0801e4c77
jiejia <jiejia2009@gmail.com>
2026-03-28 11:17:12 +0800

    Remove unused tag display code from entry meta

--- resources/views/partials/entry-meta.php
+++ resources/views/partials/entry-meta.php
@@ -1,18 +1,3 @@
-@php
-    $tags = get_the_tags();
-@endphp
-
-@if($tags && !empty($tags))
-<div class="grid grid-flow-row gap-2 mt-2">
-    <ul class="flex flex-wrap gap-2">
-        @foreach($tags as $tag)
-        <li>
-            <a href="{{ get_tag_link($tag->term_id) }}" class="text-xs text-base-content/50 bg-base-200/50 hover:bg-base-200 rounded-full py-0.5 px-2"># {{ $tag->name }}</a>
-        </li>
-        @endforeach
-    </ul>
-</div>
-@endif
 <?php
 /**
  * Entry Meta Partial

136107d1c1a7e09dd592770042a23e991d20a529
jiejia <jiejia2009@gmail.com>
2026-04-22 16:57:01 +0800

    Update plugin slug and name for consistency

--- src/Constants/PodcastPluginConstant.php
+++ src/Constants/PodcastPluginConstant.php
@@ -5,8 +5,8 @@ namespace ARippleSong\Themes\Daisy\Constants;
 class PodcastPluginConstant
 {
     public const PODCAST_POST_TYPE = 'ars_episode';
-    public const PLUGIN_SLUG = 'a-ripple-song-podcast';
-    public const PLUGIN_NAME = 'A Ripple Song Podcast';
+    public const PLUGIN_SLUG = 'a-ripple-song';
+    public const PLUGIN_NAME = 'A Ripple Song';
     public const PODCAST_TAXONOMY_SLUG = 'ars_episode_category';
     public const PODCAST_TAXONOMY_NAME = 'Episode Category';
 }

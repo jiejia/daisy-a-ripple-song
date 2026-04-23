82796b9907f0e51b3ac7ddd305cdad6a9daa275c
jiejia <jiejia2009@gmail.com>
2026-04-23 11:51:42 +0800

    Remove old zip file before creating a new build for "Daisy A Ripple Song"

--- bin/build-dist.sh
+++ bin/build-dist.sh
@@ -90,6 +90,7 @@ if [[ -z "${ZIP_NAME}" ]]; then
 fi
 
 ZIP_PATH="${BUILD_DIR}/${ZIP_NAME}"
+rm -f "${BUILD_DIR}/a-ripple-song.zip"
 rm -f "${ZIP_PATH}"
 
 (cd "${DIST_ROOT}" && zip -qr "${ZIP_PATH}" "daisy-a-ripple-song")
